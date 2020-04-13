9e403d2d653018065e7e159743c6cb965a58994b
takn <nelson.ramirez@block.one>
2020-04-13 03:41:48 -0400

    initial commit, use jUnit 5

---
+++ app/src/main/java/com/one/r/studio/ui/main/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.one.r.studio.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    fun getHeadBlock(): String {
+        return "12345";
+    }
+}
---
+++ app/src/test/java/com/one/r/studio/MainViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.one.r.studio
+
+import com.one.r.studio.ui.main.MainViewModel
+import org.junit.jupiter.api.Assertions.assertEquals
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MainViewModelTest {
+    private val sut = MainViewModel()
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun sanity(){
+        assertEquals("12345",sut.getHeadBlock());
+
+    }
+}
